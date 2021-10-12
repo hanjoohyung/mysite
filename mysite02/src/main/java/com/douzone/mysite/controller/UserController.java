42a111920c795121172c91c6c222f580ebd36b61
hanjoohyung <ghd167@naver.com>
2021-10-12 12:19:00 +0900

    user service

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -10,15 +10,24 @@ import javax.servlet.http.HttpServletResponse;
 import com.douzone.web.util.MvcUtil;
 
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
-		MvcUtil.forword("/WEB-INF/views/main/index.jsp", request, response);
+		String action = request.getParameter("a");
+		if("joinform".equals(action)) {
+		MvcUtil.forword("/WEB-INF/views/user/joinform.jsp", request, response);
+		} else if("join".equals(action)) {
+			MvcUtil.forword("/WEB-INF/views/user/joinsuccess.jsp", request, response);
+		} else if("loginform".equals(action)) {
+			MvcUtil.forword("/WEB-INF/views/user/loginform.jsp", request, response);
+		} else {
+			MvcUtil.redirect("mysite02", request, response);
+		}
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
