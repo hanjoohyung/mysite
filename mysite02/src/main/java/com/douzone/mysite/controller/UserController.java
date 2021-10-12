fb269ed1b204b5901fc0ee8b2e195cef5553722d
hanjoohyung <ghd167@naver.com>
2021-10-12 13:50:35 +0900

    Action & ActionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -7,28 +7,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.web.util.MvcUtil;
-
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+
+		String actionName = request.getParameter("a");
 		
-		String action = request.getParameter("a");
-		if("joinform".equals(action)) {
-		MvcUtil.forword("/WEB-INF/views/user/joinform.jsp", request, response);
-		} else if("join".equals(action)) {
-			MvcUtil.forword("/WEB-INF/views/user/joinsuccess.jsp", request, response);
-		} else if("login".equals(action)) {
-			MvcUtil.forword("/WEB-INF/views/user/loginform.jsp", request, response);
-		} else {
-			MvcUtil.redirect("mysite02", request, response);
-		}
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request,response);
+//		if ("joinform".equals(action)) {
+//			MvcUtil.forword("/WEB-INF/views/user/joinform.jsp", request, response);
+//		} else if ("join".equals(action)) {
+//			MvcUtil.forword("/WEB-INF/views/user/joinsuccess.jsp", request, response);
+//		} else if ("login".equals(action)) {
+//			MvcUtil.forword("/WEB-INF/views/user/loginform.jsp", request, response);
+//		} else {
+//			MvcUtil.redirect("mysite02", request, response);
+//		}
+		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
